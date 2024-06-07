5f6c939fca1f77927078fe679347b22242029951
FolashadeOmoloja <omolojashade@gmail.com>
2024-06-07 10:25:33 +0100

    Navbar started

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+
+import { NavLinks } from "@/utilities/constants"
+import Image from "next/image"
+
+
+
+const Navbar = () => {
+
+  return (
+    <nav className="flex px-[2.5rem] max-lg:justify-between max-lg:px-[2rem] max-xsm:px-[1rem] ">
+       <div>
+          <Image 
+          src={"/fracklogo.svg"} alt={"logo"} width={125} height={125} className="max-xsm:w-[100px] max-xsm:h-[100px]"/>
+       </div>
+       <ul className='flex-1 flex justify-center items-center gap-10 max-lg:hidden'>
+          {NavLinks.map((item) => (
+            <li></li>
+            // <li key={item.label} className="nav-links">
+            //   <a
+            //     href={item.href}
+            //     className='font-montserrat leading-normal text-lg text-slate-gray'
+            //   >
+            //     {item.label}
+            //   </a>
+            // </li>
+          ))}
+        </ul>
+        <div>
+            {/* <BookRideButton className="mt-10 me-6 max-xsm:me-2 max-xsm:mt-8" /> */}
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ utilities/constants/index.ts
@@ -0,0 +1,64 @@
+export const NavLinks = [
+    { navItem: "For Companies", 
+        href: "nil",
+       dropDown: true, 
+       dropDownOpt: {
+       upperSection: {
+           icon: '',
+           title: '',
+           desc: '',
+           href: ''
+        },
+       lowerSection: {
+            icon: '',
+            title: '',
+            desc: '',
+            href: ''
+         }
+       }
+    },
+    { navItem: "For Talents", 
+        href: "nil",
+       dropDown: true, 
+       dropDownOpt: {
+       upperSection: {
+           icon: '',
+           title: '',
+           desc: '',
+           href: ''
+        },
+       lowerSection: {
+            icon: '',
+            title: '',
+            desc: '',
+            href: ''
+         }
+       }
+    },
+    { navItem: "About Us", 
+        href: "about-us",
+       dropDown: false, 
+    },
+    { navItem: "Resources", 
+        href: "nil",
+       dropDown: true, 
+       dropDownOpt: {
+       upperSection: {
+           icon: '',
+           title: '',
+           desc: '',
+           href: ''
+        },
+       lowerSection: {
+            icon: '',
+            title: '',
+            desc: '',
+            href: ''
+         }
+       }
+    },
+    { navItem: "Contact Us", 
+        href: "contact-us",
+       dropDown: false, 
+    },
+];
