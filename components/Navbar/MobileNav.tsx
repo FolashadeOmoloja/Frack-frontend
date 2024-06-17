0a115ee8609762342d84b7c210fe1dc57b79f850
FolashadeOmoloja <omolojashade@gmail.com>
2024-06-17 12:59:16 +0100

    work done on navbar

--- components/Navbar/MobileNav.tsx
+++ components/Navbar/MobileNav.tsx
@@ -10,54 +10,22 @@ const MobileNav = () => {
     const [isOpenDropdown, setisOpenDropdown] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleSidebar = () => {
-      setIsOpen(!isOpen);
-    };
-
     const toggleDropdown = () => {
       setisOpenDropdown(!isOpenDropdown);}
   return (
    <nav className='lg:hidden '>
-         {
-           isOpen? (
-            <div
-            className={`${
-              isOpen ? 'translate-x-0' : '-translate-x-full'
-            } fixed inset-y-0 left-0 w-[90%] bg-color shadow-lg z-40 transform transition-transform duration-300 ease-in-out`}
-          >
-            <button
-              onClick={toggleSidebar}
-              className="absolute top-0 right-0 mt-[39px] mr-[51px]"
-            >
-              <ImCross/>
-            </button>
-            
-            <section className='flex flex-col gap-[10px]  mt-[0px] ml-[47px]'>
-            <ul >
-                 {
-                   NavLinks.map((item,index) =>{
-                     return(
-                       <a href={item.href} key={index} >
-                       {/* <li className="mobilenav-hover flex justify-between w-[210px] relative text-gray-700 mb-[20px] transition duration-300 rounded-lg pl-[10px] " key={index}>
-                         <div className="flex gap-2 ">
-                          <div className="p-2 border-[1px] border-gray-300 rounded-lg flex items-center item-icon "><span>{item.icon}</span></div>
-                         <span className="flex items-center">{item.page}</span>
-                         </div>
-                       </li> */}
-                       </a>
-                     )
-                   })
-                 }
-          </ul>
-    
-            </section>
-          </div>
-           ) :
-           (
+
             <section className='flex justify-between items-center h-24 px-9 max-sm:px-4 max-sm:flex-row-reverse'>
-              <button onClick={toggleSidebar}>
-                 <HiMiniBars3BottomLeft className='text-3xl text-[#000080] ' />  
-              </button>
+              {
+                isOpen ? (
+                     <button onClick={()=>setIsOpen(!isOpen)} >
+                        <ImCross className='text-3xl text-[#000080] '/>
+                      </button>
+                ) :   
+                <button  onClick={()=>setIsOpen(!isOpen)}>
+                     <HiMiniBars3BottomLeft className='text-3xl text-[#000080] ' />  
+                </button>
+              }
               <div>
                   <img src="/images/homepage/fracklogo.png" alt="logo" />
               </div>
@@ -78,10 +46,42 @@ const MobileNav = () => {
                  </li>
              </ul>
             </section>
-           )
-         }
    </nav>
-  )
+  //           {
+  //             isOpen? (
+  //              <div
+  //              className={`${
+  //                isOpen ? 'translate-x-0' : '-translate-x-full'
+  //              } fixed inset-y-0 left-0 w-[90%] bg-color shadow-lg z-40 transform transition-transform duration-300 ease-in-out`}
+  //            >
+  //              <button
+  //                onClick={toggleSidebar}
+  //                className="absolute top-0 right-0 mt-[39px] mr-[51px]"
+  //              >
+  //                <ImCross/>
+  //              </button>
+               
+  //              <section className='flex flex-col gap-[10px]  mt-[0px] ml-[47px]'>
+  //              <ul >
+  //                   {
+  //                     NavLinks.map((item,index) =>{
+  //                       return(
+  //                         <a href={item.href} key={index} >
+  //                         {/* <li className="mobilenav-hover flex justify-between w-[210px] relative text-gray-700 mb-[20px] transition duration-300 rounded-lg pl-[10px] " key={index}>
+  //                           <div className="flex gap-2 ">
+  //                            <div className="p-2 border-[1px] border-gray-300 rounded-lg flex items-center item-icon "><span>{item.icon}</span></div>
+  //                           <span className="flex items-center">{item.page}</span>
+  //                           </div>
+  //                         </li> */}
+  //                         </a>
+  //                       )
+  //                     })
+  //                   }
+  //            </ul>
+       
+  //              </section>
+  //            </div>
+   )
 }
 
 export default MobileNav
