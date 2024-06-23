2ea9dd6849131144f86d7bdbe48b245298cce325
FolashadeOmoloja <omolojashade@gmail.com>
2024-06-23 14:01:01 +0100

    testimonials section updated

--- components/Offer/Testimonials.tsx
+++ components/Offer/Testimonials.tsx
@@ -26,8 +26,8 @@ const testimonials = [
 
 const Testimonials = () => {
   return (
-    <section className="section-container flex flex-col">
-      <span className='max-w-[432px] font-bold leading-10 text-[38px]'>
+    <section className="section-container flex  max-xslg:gap-7 max-slg:gap-14  max-slg:flex-col justify-between max-slg:items-center">
+      <span className='max-w-[432px] font-bold leading-10 text-[38px] max-slg:text-center max-md:text-[24px]'>
         What our clients say about Frack.
       </span>
       <Swiper
@@ -39,12 +39,12 @@ const Testimonials = () => {
           prevEl: '.swiper-button-prev',
         }}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
-        className="max-w-[632px] mt-8"
+        className="max-w-[632px] relative h-[250px] max-md:w-full"
       >
         {testimonials.map((testimonial, index) => (
           <SwiperSlide key={index}>
-            <section className='flex flex-col'>
-              <p className='mb-5 leading-[29.4px] text-left tracking-[0.05em]'>
+            <section className='flex flex-col max-md:text-wrap'>
+              <p className='mb-5 leading-[29.4px] text-left tracking-[0.05em] max-sm:text-sm'>
                 {testimonial.text}
               </p>
               <span className='font-bold mb-1'>
@@ -56,11 +56,11 @@ const Testimonials = () => {
             </section>
           </SwiperSlide>
         ))}
-        <div className="flex justify-end gap-5 text-2xl mt-12">
-          <button className="swiper-button-prev">
+        <div className="flex  gap-5 md:text-2xl text-lg mt-4 absolute bottom-0 right-0 max-slg:left-[40%] z-20">
+          <button className="swiper-button-prev cursor-pointer">
             <FaArrowLeft />
           </button>
-          <button className="swiper-button-next">
+          <button className="swiper-button-next cursor-pointer">
             <FaArrowRight />
           </button>
         </div>
