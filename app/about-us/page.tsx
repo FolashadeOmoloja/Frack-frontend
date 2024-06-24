74be2b3c80fb73870361255ab403e773d9cf119c
FolashadeOmoloja <omolojashade@gmail.com>
2024-06-24 12:12:58 +0100

    changes made

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,8 +2,10 @@ import Header from "@/components/Herosection/Header";
 import Navbar from "@/components/Navbar/NavBar";
 
 export default function AboutPage()  {
-<>
+return (
+    <>
 <Navbar activeItem={2}/>
-<Header/>
+
 </>
+)
 }
