abd4491a6725a3568e0ebacca765e757ea4f044a
FolashadeOmoloja <omolojashade@gmail.com>
2024-06-27 14:56:24 +0100

    changes made to maincontent, testimonials, cta

--- components/Contact/Form.tsx
+++ components/Contact/Form.tsx
@@ -2,8 +2,30 @@ import CTABTN from "../Elements/CTA/CTA-Button";
 
 const Form = () => {
   return (
-    <form action="">
-      <CTABTN route={""} />
+    <form action="" className="">
+      <section>
+        <div className="flex flex-col gap-2 w-full">
+          <label htmlFor="name" className="font-semibold text-sm leading-4">
+            Name (required)
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            required
+            className="w-full h-[47px] rounded-md py-[10px] px-4 bg-white outline-none text-black placeholder:text-[#696969]"
+            placeholder="Your name"
+          />
+        </div>
+      </section>
+      <CTABTN
+        route={""}
+        CTA="Send message"
+        showIcon
+        color="text-black"
+        backGround="bg-white"
+        width="w-[180px]"
+      />
     </form>
   );
 };
--- components/Contact/MainContent.tsx
+++ components/Contact/MainContent.tsx
@@ -1,9 +1,13 @@
 import CTA from "../LandingPage/Offer/CTA";
 import Testimonials from "../LandingPage/Offer/Testimonials";
+import Form from "./Form";
 
 const MainContent = () => {
   return (
-    <main className="bg-[#000080] mt-[151px] max-slg:mt-[50px] pt-[151px] max-slg:pt-[50px] relative">
+    <main className="bg-[#000080] mt-[151px] max-slg:mt-[50px] pt-1">
+      <section className="section-container flex text-white">
+        <Form />
+      </section>
       <Testimonials color="text-white" />
       <section className=" relative top-14 mb-[150px] max-slg:mb-[50px] ">
         <CTA
