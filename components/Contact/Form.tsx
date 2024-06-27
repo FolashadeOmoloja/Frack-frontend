abaf7bd2e58477f14772e7545b419743d4fb2753
FolashadeOmoloja <omolojashade@gmail.com>
2024-06-27 15:17:51 +0100

    work done on contact form

--- components/Contact/Form.tsx
+++ components/Contact/Form.tsx
@@ -3,20 +3,51 @@ import CTABTN from "../Elements/CTA/CTA-Button";
 const Form = () => {
   return (
     <form action="" className="">
-      <section>
-        <div className="flex flex-col gap-2 w-full">
-          <label htmlFor="name" className="font-semibold text-sm leading-4">
-            Name (required)
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            required
-            className="w-full h-[47px] rounded-md py-[10px] px-4 bg-white outline-none text-black placeholder:text-[#696969]"
-            placeholder="Your name"
-          />
-        </div>
+      <section className="mb-10">
+        <section className="grid  md:grid-cols-1 lg:grid-cols-3 gap-10">
+          <div className="flex flex-col gap-2 w-full md:w-[296px]">
+            <label htmlFor="name" className="font-semibold text-sm leading-4">
+              Name (required)
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              className="w-full h-[47px] rounded-md py-[10px] px-4 bg-white outline-none text-black placeholder:text-[#696969]"
+              placeholder="Your name"
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full md:w-[296px]">
+            <label
+              htmlFor="company-name"
+              className="font-semibold text-sm leading-4"
+            >
+              Company (optional)
+            </label>
+            <input
+              type="text"
+              name="company-name"
+              id="company-name"
+              required
+              className="w-full h-[47px] rounded-md py-[10px] px-4 bg-white outline-none text-black placeholder:text-[#696969]"
+              placeholder="Your Company's name"
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full md:w-[296px]">
+            <label htmlFor="email" className="font-semibold text-sm leading-4">
+              Email (required)
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              className="w-full h-[47px] rounded-md py-[10px] px-4 bg-white outline-none text-black placeholder:text-[#696969]"
+              placeholder="Your Email"
+            />
+          </div>
+        </section>
       </section>
       <CTABTN
         route={""}
--- components/Contact/MainContent.tsx
+++ components/Contact/MainContent.tsx
@@ -5,7 +5,13 @@ import Form from "./Form";
 const MainContent = () => {
   return (
     <main className="bg-[#000080] mt-[151px] max-slg:mt-[50px] pt-1">
-      <section className="section-container flex text-white">
+      <section className="section-container flex max-lg:flex-col text-white gap-10">
+        <div>
+          <h3 className="font-bold text-2xl">Talk to us.</h3>
+          <p className="mt-4 text-wrap">
+            Fill in this form or send us an e-mail with your inquiry.
+          </p>
+        </div>
         <Form />
       </section>
       <Testimonials color="text-white" />
