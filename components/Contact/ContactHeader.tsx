014ff3a90ea6a8416fb633ccb9380c6eab16f1ba
FolashadeOmoloja <omolojashade@gmail.com>
2024-06-29 05:05:11 +0100

    success page updated

--- components/Contact/ContactHeader.tsx
+++ components/Contact/ContactHeader.tsx
@@ -3,12 +3,9 @@ const ContactHeader = () => {
     <header className="section-container flex max-slg:flex-col justify-between items-center">
       <div className="flex flex-col max-slg:items-center max-slg:mb-6 slg:ml-[5px]">
         <span className="text-[#161519] headingspan mb-0">Let’s talk!</span>
-        <p className="font-bold text-[72px] max-xslg:text-[52px] leading-[52px] text-[#111013]  max-md:text-[38px] max-sm:text-3xl  max-xsm:text-center text-wrap">
-          How can we{" "}
-          <span className="bg-gradient-to-r from-[#000080] via-[#1995CF]  to-[#1995CF] text-transparent bg-clip-text max-xslg:mt-5">
-            help ?
-          </span>
-        </p>
+        <h2 className="header-headline">
+          How can we <span className="header-headline-span">help ?</span>
+        </h2>
       </div>
       <div className="rounded-xl basis-[40%]">
         <img
--- components/SuccessStory/Header.tsx
+++ components/SuccessStory/Header.tsx
@@ -1,8 +1,34 @@
 const Header = () => {
   return (
     <header className="section-container flex justify-between items-center max-slg:flex-col gap-10">
-      <section></section>
-      <section></section>
+      <section className="flex flex-col max-slg:items-center max-slg:mb-6 slg:ml-[5px] text-[#161519] ">
+        <span className=" headingspan mb-0">Success Stories</span>
+        <h2 className="header-headline ">
+          Real Results, Real{" "}
+          <span className="header-headline-span">Impact</span>
+        </h2>
+        <p className="leading-[29.4px] tracking-[1.5%] max-slg:text-center">
+          Welcome to our <span className="font-bold">Success Stories page</span>
+          , where we celebrate the achievements of our clients and showcase the
+          real results and impact of partnering with Frack.
+        </p>
+      </section>
+      <section className="flex gap-10">
+        <div className="xlg:w-[290px] xlg:h-[513px]">
+          <img
+            src="/images/homepage/iPhone.svg"
+            alt="img"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="xlg:w-[290px] xlg:h-[513px]">
+          <img
+            src="/images/homepage/iPhone1.svg"
+            alt="img"
+            className="w-full h-full"
+          />
+        </div>
+      </section>
     </header>
   );
 };
