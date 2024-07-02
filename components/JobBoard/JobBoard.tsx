7e905dc3f14d26967e65d46cf5370cbee13db224
FolashadeOmoloja <omolojashade@gmail.com>
2024-07-02 23:51:43 +0100

    job board added

--- components/JobBoard/JobBoard.tsx
+++ components/JobBoard/JobBoard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const JobBoard = () => {
   return (
     <section className="section-container bg-[#F4F5F7] mt-0 h-[calc(100vh-96px)] py-7">
-      <section className="flex flex-col md:flex-row md:h-full">
+      <section className="flex flex-col md:flex-row md:h-full gap-10">
         <div className="flex-shrink-0 md:w-1/3 lg:w-1/4 h-64 md:h-full bg-white p-5 shadow-md md:sticky md:top-0">
           left-side
         </div>
