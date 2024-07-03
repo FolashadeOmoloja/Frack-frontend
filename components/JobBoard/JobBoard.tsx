2d1221635a6b18556ccfa2f0e543a94a9f3d0ad5
FolashadeOmoloja <omolojashade@gmail.com>
2024-07-03 19:52:08 +0100

    jobpostings added

--- components/JobBoard/JobBoard.tsx
+++ components/JobBoard/JobBoard.tsx
@@ -10,7 +10,7 @@ const JobBoard = () => {
         <div className="md:sticky md:top-[40px] h-[calc(100vh-50px)] md:h-[calc(100vh-80px)] overflow-y-auto flex-shrink-0 md:w-1/3 lg:w-1/4 bg-white p-5 shadow-md rounded-sm custom-scrollbar">
           <Filter />
         </div>
-        <div className="flex-grow p-5 overflow-y-auto ">
+        <div className="flex-grow px-5 pb-5 overflow-y-auto ">
           <JobPosting />
         </div>
       </section>
