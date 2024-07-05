1c95d5220b1388437841e71e5c3d7bcf190723ac
FolashadeOmoloja <omolojashade@gmail.com>
2024-07-05 03:03:37 +0100

    hire-talent page

---
+++ components/HireTalent/Login/Login.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <section className="h-[100vh] flex">
+      <div
+        className="basis-1/2 bg-cover bg-no-repeat bg-[#000080] relative"
+        style={{ backgroundImage: "url('/images/homepage/signup-bg1.svg')" }}
+      >
+        <p className="text-white max-w-[340px] leading-[29px] tracking-[1.5%] absolute right-0 mx-10 top-[270px]">
+          companies can simply post their requirements on our job board, and
+          within moments,they'll be matched with a curated list of highly
+          qualified professionals from our extensive network.
+        </p>
+      </div>
+      <form action="" method="post"></form>
+    </section>
+  );
+};
+
+export default Login;
