65255223ea069dc5f72139d3bf157c8f6911e26d
FolashadeOmoloja <omolojashade@gmail.com>
2024-07-18 17:12:59 +0100

    changes made to job board

--- components/JobBoard/JobBoard.tsx
+++ components/JobBoard/JobBoard.tsx
@@ -4,43 +4,43 @@ import JobSearchBar from "../Elements/JobSearchBar";
 import Filter from "./Filter";
 import JobPosting from "./JobPostings";
 import { jobPostings } from "@/utilities/constants/jobData";
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 
-type filteredJobs = [
-  {
-    title: string;
-    company: string;
-    location: string;
-    priceRange: string;
-    jobProximity: string;
-    jobHours: string;
-    experience: string;
-    skills: string[];
-    role: string;
-    country: string;
-  }
-];
+type Job = {
+  title: string;
+  company: string;
+  location: string;
+  priceRange: string;
+  jobProximity: string;
+  jobHours: string;
+  experience: string;
+  skills: string[];
+  role: string;
+  country: string;
+};
+
+type FilteredJobs = Job[];
 
 type IsCheckedState = {
   [key: number]: boolean;
 };
 
 const JobBoard = () => {
-  const [filteredJobs, setFilteredJobs] = useState(jobPostings);
-  const [newJobPosting, setNewjobposting] = useState(jobPostings);
+  const [filteredJobs, setFilteredJobs] = useState<FilteredJobs>(jobPostings);
+  const [newJobPosting, setNewJobPosting] = useState<FilteredJobs>(jobPostings);
   const [isChecked, setIsChecked] = useState<IsCheckedState>({});
   const [showFilter, setShowFilter] = useState(false);
 
   return (
     <section className="section-container bg-[#F4F5F7] mt-0 py-7">
       <JobSearchBar
+        onNewSearch={setNewJobPosting}
         onSearch={setFilteredJobs}
-        onNewSearch={setNewjobposting}
-        jobPosting={jobPostings as filteredJobs}
+        jobPosting={jobPostings}
         changeIsCheck={setIsChecked}
       />
       <button
-        className="text-2xl text-[#000080] mb-5 sm:hidden bg-white w-full h-[50px] rounded-lg          flex justify-center items-center shadow-md"
+        className="text-2xl text-[#000080] mb-5 sm:hidden bg-white w-full h-[50px] rounded-lg flex justify-center items-center shadow-md"
         onClick={() => setShowFilter(!showFilter)}
       >
         <FaFilter />
@@ -49,16 +49,13 @@ const JobBoard = () => {
         <div className={`filterDiv max-sm:${showFilter ? "" : "hidden"}`}>
           <Filter
             onFilter={setFilteredJobs}
-            jobPostings={newJobPosting as filteredJobs}
+            jobPostings={newJobPosting}
             isChecked={isChecked}
             changeIsCheck={setIsChecked}
           />
         </div>
-        <div className="flex-grow md:pl-5 pb-5 overflow-y-auto ">
-          <JobPosting
-            filteredJobs={filteredJobs as filteredJobs}
-            jobPostings={jobPostings as filteredJobs}
-          />
+        <div className="flex-grow md:pl-5 pb-5 overflow-y-auto">
+          <JobPosting filteredJobs={filteredJobs} jobPostings={jobPostings} />
         </div>
       </section>
     </section>
