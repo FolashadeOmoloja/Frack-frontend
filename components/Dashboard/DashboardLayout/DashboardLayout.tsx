64595943629c35d948be05f2c3f91796c60d59c5
FolashadeOmoloja <omolojashade@gmail.com>
2024-07-20 06:07:22 +0100

    work done on dashboard

--- components/Dashboard/DashboardLayout/DashboardLayout.tsx
+++ components/Dashboard/DashboardLayout/DashboardLayout.tsx
@@ -1,3 +1,4 @@
+import DashCards from "./DashCards";
 import StatsCard from "./StatsCard";
 type analytics = {
   analtyticsTitle: string;
@@ -12,13 +13,35 @@ const DashboardLayout = ({
   analytics: analytics;
 }) => {
   return (
-    <section className="dashboard-container flex">
-      <section className="bg-white pt-14 px-9 pb-20 rounded-md basis-[75%]">
+    <section className="dashboard-container flex gap-10 max-md:flex-col">
+      <section className="bg-white pt-14 px-9 max-sm:px-[10px] pb-20 rounded-md md:basis-[75%]">
         <h2 className="text-2xl font-bold mb-1">Your dashboard info</h2>
         <span className="text-[#7C8698] ">{dashInfo}</span>
         <StatsCard analytics={analytics} />
       </section>
-      <aside></aside>
+      <aside className="md:basis-[25%] flex flex-col gap-7">
+        <DashCards
+          gif="/images/dashboard/profile.gif"
+          title={"Profile Status:"}
+          status={"Recruit"}
+          link={"hire-talent/dashboard/edit-profile"}
+          linkName={"Edit Profile"}
+        />
+        <DashCards
+          gif="/images/dashboard/suitcase.gif"
+          title={"Work mode preference"}
+          status={"Remote/hybrid"}
+          link={"hire-talent/dashboard/my-jobs"}
+          linkName={"Check My Jobs"}
+        />
+        <DashCards
+          gif="/images/dashboard/message.gif"
+          title={"Have an idea you’d like to share?"}
+          status={""}
+          link={"hire-talent/dashboard/contact"}
+          linkName={"Send us a message"}
+        />
+      </aside>
     </section>
   );
 };
--- components/Dashboard/DashboardLayout/StatsCard.tsx
+++ components/Dashboard/DashboardLayout/StatsCard.tsx
@@ -5,16 +5,16 @@ type analytics = {
 }[];
 const StatsCard = ({ analytics }: { analytics: analytics }) => {
   return (
-    <section className="mt-8  grid grid-cols-1 md:grid-cols-2 gap-8">
+    <section className="mt-8  grid grid-cols-1 slg:grid-cols-2 gap-8">
       {analytics.map((item, idx) => (
         <div
           className={`col-span-1 ${
             idx === 0 ? "bg-[#000080] text-white" : "border border-[#E0E1E6] "
-          } h-[277px] w-full rounded-md pt-14 pb-16 pl-9 flex flex-col`}
+          } h-[240px] w-full rounded-md py-12  pl-9 flex flex-col gap-5`}
           key={idx}
         >
-          <span className="font-bold text-lg mb-8">{item.analtyticsTitle}</span>
-          <span className="text-5xl font-bold mb-5">{item.stats}</span>
+          <span className="font-bold text-lg ">{item.analtyticsTitle}</span>
+          <span className="text-5xl font-bold ">{item.stats}</span>
           <span
             className={`${
               idx === 0 ? "text-white" : "text-[#7C8698]"
