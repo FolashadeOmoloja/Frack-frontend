f1d56fe106f7fc3076b699bbb050a765bb049b92
FolashadeOmoloja <omolojashade@gmail.com>
2024-08-26 20:40:11 +0100

    new commit

--- components/LandingPage/Offer/Footer.tsx
+++ components/LandingPage/Offer/Footer.tsx
@@ -4,7 +4,9 @@ const Footer = () => {
       <div className="grid grid-cols-1 lg:grid-cols-4 xslg:gap-8 gap-6">
         {/* Column 1 */}
         <div className="col-span-1">
-          <img src="/images/homepage/fracklogo.png" alt="Footer Logo" />
+          <a href="/">
+            <img src="/images/homepage/fracklogo.png" alt="Footer Logo" />
+          </a>
           <p className="text-sm text-[#000080] mt-8 font-[600] leading-[26.4px]">
             Join the ranks of companies revolutionizing the way they work with
             talent. Experience the future of hiring with Frack today.
@@ -20,15 +22,20 @@ const Footer = () => {
                 <li className="hover:text-black">
                   <a href="/hire-talent">Hire Talent</a>
                 </li>
-                <li className="hover:text-black">Success Stories</li>
+                <li className="hover:text-black">
+                  <a href="/success-story">Success Stories</a>
+                </li>
               </ul>
             </li>
             <li className="text-lg text-[#000080] font-bold transition cursor-pointer">
               <span className="hover:text-black ">For Talent</span>
               <ul className="font-semibold text-sm mt-6 flex flex-col gap-1">
-                <li className="hover:text-black ">Find Job</li>
-                <li className="hover:text-black ">Sign Up</li>
-                <li className="hover:text-black ">Resources</li>
+                <li className="hover:text-black ">
+                  <a href="/jobs"> Find Job</a>
+                </li>
+                <li className="hover:text-black ">
+                  <a href="/sign-up">Sign Up</a>
+                </li>
               </ul>
             </li>
           </ul>
@@ -37,17 +44,30 @@ const Footer = () => {
         {/* Column 3 */}
         <div className="col-span-1">
           <ul>
-            <li className="text-lg text-[#000080] font-bold transition mb-10 ">
+            <li className="text-lg text-[#000080] font-bold transition mb-8 ">
               <span>About Us</span>
               <ul className="font-semibold text-sm mt-6">
-                <li className="hover:text-black cursor-pointer">About Frack</li>
+                <li className="hover:text-black cursor-pointer">
+                  <a href="/about-us">About Frack</a>
+                </li>
               </ul>
             </li>
-            <li className="text-lg text-[#000080] font-bold transition ">
+            <li className="text-lg text-[#000080] font-bold transition mb-8">
               <span>Contact</span>
               <ul className="font-semibold text-sm mt-6">
                 <li className="hover:text-black cursor-pointer">
-                  Contact Frack
+                  <a href="/contact-us">Contact Frack</a>
+                </li>
+              </ul>
+            </li>
+            <li className="text-lg text-[#000080] font-bold transition cursor-pointer">
+              <span className="hover:text-black ">Resources</span>
+              <ul className="font-semibold text-sm mt-6 flex flex-col gap-1">
+                <li className="hover:text-black ">
+                  <a href="/blog">Blog</a>
+                </li>
+                <li className="hover:text-black ">
+                  <a href="/faq">Faq</a>
                 </li>
               </ul>
             </li>
